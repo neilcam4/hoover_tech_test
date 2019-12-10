f7b9e5f52f2f59d79cd0fc11156125406013c1d9
Neil Campbell <neildcampbell@hotmail.com>
2019-12-10 10:16:35 +0000

    added consol.logs for each output

--- src/hoover.js
+++ src/hoover.js
@@ -10,31 +10,31 @@ DIRT_PATCHES = 0;
 
 Hoover.prototype.clean = function(){
     this.dirtCount++;
-    return this.dirtCount;
+    console.log(this.dirtCount) 
 }
 Hoover.prototype.north = function(){
     if(this.Y < MAX_COORDINATE){
         this.Y++
     } 
-    return `${this.X} ${this.Y}`
+    console.log(`${this.X} ${this.Y}`)
 }
 Hoover.prototype.south = function(){
     if(this.Y > MIN_COORDINATE){
         this.Y--
     } 
-      return `${this.X} ${this.Y}`
+    console.log(`${this.X} ${this.Y}`)
 }
 Hoover.prototype.east = function(){
     if(this.X < MAX_COORDINATE){
         this.X++
     }
-    return `${this.X} ${this.Y}`
+    console.log(`${this.X} ${this.Y}`)
 }
 Hoover.prototype.west = function(){
     if(this.X > MIN_COORDINATE){
         this.X--
     } 
-    return `${this.X} ${this.Y}`
+    console.log(`${this.X} ${this.Y}`)
 }
 Hoover.prototype.dirt = function(){
     this.dirtPresence = true;
