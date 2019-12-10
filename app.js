33c6a7a38d7c7fe7dcf5db0485e5ea50852d2159
Neil Campbell <neildcampbell@hotmail.com>
2019-12-10 22:54:11 +0000

    refactor comment lines

--- app.js
+++ app.js
@@ -37,11 +37,6 @@ reader.onload = function(){
     console.log(`third dirt = ${dirt3X}, ${dirt3Y}`)
     console.log(`instructions = ${instructions}`)
     console.log(instructionsArray)
-    // console.log("size X = " + sizeRectangleX)
-    // console.log("starting X = " + startingPositionX)
-    // console.log("starting Y = " + startingPositionY)
-
-
 }
 reader.readAsText(input.files[0])
 },false)
@@ -93,8 +88,7 @@ Hoover.prototype.clean = function(){
     if(this.dirtPresence == true){
         this.dirtCount++;
         this.dirtPresence = false;
-    }
-    // console.log(this.dirtCount) 
+    } 
     console.log("clean occurred")
 }
 Hoover.prototype.north = function(){
@@ -155,49 +149,3 @@ function startHoover(){
     console.log(`${hoover.X} ${hoover.Y}`)
     console.log(hoover.dirtCount)
 }
-// document.getElementById("button1").addEventListener('click', getInput);
-// var instructions;
-// var sizeRectangleX;
-// let startingPositionX;
-// function getInput(){
-//     fetch('input.txt')
-//     .then(function(res){
-//         console.log(res.clone().text())
-//         return res.clone().text();
-//     })
-//     .then(function(data){
-//         console.log("data " + data)
-//         document.getElementById('output').innerHTML = data
-//         let dataSplit = data.split('/n').join(" ")
-//         console.log("dataSplit " + dataSplit)
-//         let output = data.replace(/(\r\n|\n|\r)/gm,"").split(" ")
-//         instructions = output[output.length - 1]
-       
-//         console.log("output " + output)
-//         let array = output.slice(0,-1)
-//         console.log(data.split(""))
-//         let newData = data.split("")
-//         console.log(newData)
-//         console.log("array " + array) 
-//         sizeRectangleX = newData[0]
-//         document.getElementById('sizeRectangleX').innerHTML = `Size of Rectangle X Value = ${sizeRectangleX}`
-//         let sizeRectangleY = newData[2]
-//         document.getElementById('sizeRectangleY').innerHTML = `Size of Rectangle Y Value =  ${sizeRectangleY}`
-//         startingPositionX = newData[4]
-//         document.getElementById('startingPositionX').innerHTML = `Starting Position X Value =  ${startingPositionX}`
-//         let startingPositionY = newData[6]
-//         document.getElementById('startingPositionY').innerHTML = `Starting Position X Value =  ${startingPositionY}`
-//         let dirt1X = newData[8]
-//         let dirt1Y = newData[10]
-//         document.getElementById('dirt1').innerHTML = `Dirt found at =  (${dirt1X},${dirt1Y})`
-//         let dirt2X = newData[12]
-//         let dirt2Y = newData[14]
-//         document.getElementById('dirt2').innerHTML = `Dirt found at =  (${dirt2X},${dirt2Y})`
-//         let dirt3X = newData[16]
-//         let dirt3Y = newData[18]
-//         document.getElementById('dirt3').innerHTML = `Dirt found at =  (${dirt3X},${dirt3Y})`
-//         let instructionsSplit = instructions.split("")
-//         console.log(instructionsSplit)
-//         document.getElementById('instructionsSplit').innerHTML = `Instructions = ${instructionsSplit}`
-//     }) 
-// }
