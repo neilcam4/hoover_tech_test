b73e4a0563960a04bf68fdc461cbb13e9e2504df
Neil Campbell <neildcampbell@hotmail.com>
2019-12-10 17:44:27 +0000

    added filereader

--- app.js
+++ app.js
@@ -1,48 +1,116 @@
 console.log("hello from app.js")
-document.getElementById("button1").addEventListener('click', getInput);
+const input = document.querySelector('input[type="file"]')
+let output;
+let newData;
+let sizeRectangleX;
+input.addEventListener('change', function(e){
+const reader = new FileReader()
+reader.onload = function(){
+    output = reader.result
+    newData = output.split("")
+    sizeRectangleX = newData[0]
+    console.log("inputTXT = "+ output)
+    console.log("newData = "+ newData)
+    console.log("size X = " + sizeRectangleX)
 
-function getInput(){
-    fetch('input.txt')
-    .then(function(res){
-        console.log(res.clone().text())
-        return res.clone().text();
-    })
-    .then(function(data){
-        console.log("data " + data)
-        document.getElementById('output').innerHTML = data
-        let dataSplit = data.split('/n').join(" ")
-        console.log("dataSplit " + dataSplit)
-        let output = data.replace(/(\r\n|\n|\r)/gm,"").split(" ")
-        let instructions = output[output.length - 1]
-        document.getElementById('instructions').innerHTML = `Instructions = ${instructions}`
-        console.log("instructions " + instructions)
-        console.log("output " + output)
-        let array = output.slice(0,-1)
-        console.log(data.split(""))
-        let newData = data.split("")
-        console.log(newData)
-        console.log("array " + array) 
-        let sizeRectangleX = newData[0]
-        document.getElementById('sizeRectangleX').innerHTML = `Size of Rectangle X Value = ${sizeRectangleX}`
-        let sizeRectangleY = newData[2]
-        document.getElementById('sizeRectangleY').innerHTML = `Size of Rectangle Y Value =  ${sizeRectangleY}`
-        let startingPositionX = newData[4]
-        document.getElementById('startingPositionX').innerHTML = `Starting Position X Value =  ${startingPositionX}`
-        let startingPositionY = newData[6]
-        document.getElementById('startingPositionY').innerHTML = `Starting Position X Value =  ${startingPositionY}`
-        let dirt1X = newData[8]
-        let dirt1Y = newData[10]
-        document.getElementById('dirt1').innerHTML = `Dirt found at =  (${dirt1X},${dirt1Y})`
-        let dirt2X = newData[12]
-        let dirt2Y = newData[14]
-        document.getElementById('dirt2').innerHTML = `Dirt found at =  (${dirt2X},${dirt2Y})`
-        let dirt3X = newData[16]
-        let dirt3Y = newData[18]
-        document.getElementById('dirt3').innerHTML = `Dirt found at =  (${dirt3X},${dirt3Y})`
-        let instructionsSplit = instructions.split("")
-        console.log(instructionsSplit)
-        document.getElementById('instructionsSplit').innerHTML = `Instructions = ${instructionsSplit}`
-    })
-    
+}
+reader.readAsText(input.files[0])
+},false)
+
+
+MIN_COORDINATE = 0;
+MAX_COORDINATE = sizeRectangleX;
+DIRT_PATCHES = 0;
+STARTING_COORDINATE_X = 1;
+STARTING_COORDINATE_Y = 2;
+
+
+function Hoover (){
+    this.X = STARTING_COORDINATE_X;
+    this.Y = STARTING_COORDINATE_Y;
+    this.dirtCount = DIRT_PATCHES;
+    this.dirtPresence = false;
+    this.MAX_COORDINATE = sizeRectangleX;
 }
 
+Hoover.prototype.clean = function(){
+    if(this.dirtPresence == true){
+        this.dirtCount++;
+        this.dirtPresence = false;
+    }
+    console.log(this.dirtCount) 
+}
+Hoover.prototype.north = function(){
+    if(this.Y < MAX_COORDINATE){
+        this.Y++
+    } 
+    console.log(`${this.X} ${this.Y}`)
+}
+Hoover.prototype.south = function(){
+    if(this.Y > MIN_COORDINATE){
+        this.Y--
+    } 
+    console.log(`${this.X} ${this.Y}`)
+}
+Hoover.prototype.east = function(){
+    if(this.X < MAX_COORDINATE){
+        this.X++
+    }
+    console.log(`${this.X} ${this.Y}`)
+}
+Hoover.prototype.west = function(){
+    if(this.X > MIN_COORDINATE){
+        this.X--
+    } 
+    console.log(`${this.X} ${this.Y}`)
+}
+Hoover.prototype.dirt = function(){
+    this.dirtPresence = true;
+    console.log(this.dirtPresence)
+}
+// document.getElementById("button1").addEventListener('click', getInput);
+// var instructions;
+// var sizeRectangleX;
+// let startingPositionX;
+// function getInput(){
+//     fetch('input.txt')
+//     .then(function(res){
+//         console.log(res.clone().text())
+//         return res.clone().text();
+//     })
+//     .then(function(data){
+//         console.log("data " + data)
+//         document.getElementById('output').innerHTML = data
+//         let dataSplit = data.split('/n').join(" ")
+//         console.log("dataSplit " + dataSplit)
+//         let output = data.replace(/(\r\n|\n|\r)/gm,"").split(" ")
+//         instructions = output[output.length - 1]
+       
+//         console.log("output " + output)
+//         let array = output.slice(0,-1)
+//         console.log(data.split(""))
+//         let newData = data.split("")
+//         console.log(newData)
+//         console.log("array " + array) 
+//         sizeRectangleX = newData[0]
+//         document.getElementById('sizeRectangleX').innerHTML = `Size of Rectangle X Value = ${sizeRectangleX}`
+//         let sizeRectangleY = newData[2]
+//         document.getElementById('sizeRectangleY').innerHTML = `Size of Rectangle Y Value =  ${sizeRectangleY}`
+//         startingPositionX = newData[4]
+//         document.getElementById('startingPositionX').innerHTML = `Starting Position X Value =  ${startingPositionX}`
+//         let startingPositionY = newData[6]
+//         document.getElementById('startingPositionY').innerHTML = `Starting Position X Value =  ${startingPositionY}`
+//         let dirt1X = newData[8]
+//         let dirt1Y = newData[10]
+//         document.getElementById('dirt1').innerHTML = `Dirt found at =  (${dirt1X},${dirt1Y})`
+//         let dirt2X = newData[12]
+//         let dirt2Y = newData[14]
+//         document.getElementById('dirt2').innerHTML = `Dirt found at =  (${dirt2X},${dirt2Y})`
+//         let dirt3X = newData[16]
+//         let dirt3Y = newData[18]
+//         document.getElementById('dirt3').innerHTML = `Dirt found at =  (${dirt3X},${dirt3Y})`
+//         let instructionsSplit = instructions.split("")
+//         console.log(instructionsSplit)
+//         document.getElementById('instructionsSplit').innerHTML = `Instructions = ${instructionsSplit}`
+//     }) 
+// }
--- spec/hooverSpec.js
+++ spec/hooverSpec.js
@@ -1,23 +1,27 @@
 describe("Hoover", function(){
     let henry;
+    let STARTING_COORDINATE_X;
+    let STARTING_COORDINATE_Y;
     let MIN_COORDINATE;
     let MAX_COORDINATE;
     let DIRT_PATCHES;
     beforeEach(function(){
         henry = new Hoover()
+        STARTING_COORDINATE_X = 1;
+        STARTING_COORDINATE_Y = 2;
         MIN_COORDINATE = 0;
         MAX_COORDINATE = 5;
         DIRT_PATCHES = 0;
     })
-    it("describes making a new hoover at default position X =0", function(){      
-        expect(henry.X).toEqual(MIN_COORDINATE)
+    it("describes making a new hoover at starting position X = 1", function(){      
+        expect(henry.X).toEqual(STARTING_COORDINATE_X)
     })
-    it("describes making a new hoover at default position Y =0", function(){     
-        expect(henry.Y).toEqual(MIN_COORDINATE)
+    it("describes making a new hoover at starting position Y = 2", function(){     
+        expect(henry.Y).toEqual(STARTING_COORDINATE_Y)
     })
     it("describes the North instruction moves hoover 1 place up", function(){     
         henry.north()
-        expect(henry.Y).toEqual(MIN_COORDINATE + 1)
+        expect(henry.Y).toEqual(STARTING_COORDINATE_Y + 1)
     })
     it("describes the South instruction moves hoover 1 place down", function(){
         henry.Y = 4
@@ -26,7 +30,7 @@ describe("Hoover", function(){
     })
     it("describes the East instruction moves hoover 1 place right", function(){
         henry.east()
-        expect(henry.X).toEqual(MIN_COORDINATE + 1)
+        expect(henry.X).toEqual(STARTING_COORDINATE_X + 1)
     })
     it("describes the West instruction moves hoover 1 place left", function(){
         henry.X = 4
@@ -51,7 +55,7 @@ describe("Hoover", function(){
     it("describes the hoover cannot move lower than X = 0", function(){
         henry.X = MIN_COORDINATE
         henry.west()
-        expect(henry.Y).toEqual(MIN_COORDINATE)
+        expect(henry.X).toEqual(MIN_COORDINATE)
     })
     it("describes the hoover picking up a patch of dirt", function(){
         henry.dirt()
